4df0778aebacc4287305c033479904a7312831e8
TylerLaTruong <101002549+TylerLaTruong@users.noreply.github.com>
2023-11-08 14:35:44 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,1 @@
-az webapp up --name flaskappyendt --resource-group Azuredevops --sku B1 --logs --runtime PYTHON:3.9
+az webapp up --name flaskapptruonglv9 --resource-group Azuredevops --sku B1 --logs --runtime PYTHON:3.9
